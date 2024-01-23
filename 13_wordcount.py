778377ba5fe884bc4a61ecdaf3591c1757c0441e
Danilo Ferreira <danilosantos.turbo@gmail.com>
2024-01-22 23:10:10 -0300

    exercicios 22_01_2024

--- 13_wordcount.py
+++ 13_wordcount.py
@@ -57,6 +57,41 @@ import sys
 
 # +++ SUA SOLUÇÃO +++
 # Defina as funções print_words(filename) e print_top(filename).
+def print_words(filename):
+    with open(filename, 'r') as arquivo:
+        conteudo = arquivo.read()
+    l = conteudo[:].lower().split()
+    # print(l)
+    leters = []
+    nums = []
+    for w in l:
+        if w in l:
+            leters.append(w)
+            nums.append(l.count(w))
+            l1 = (sorted(set(leters)))
+            l2 = (sorted(set(nums)))
+            # print(l1)
+            # print(l2)
+            l3 = list(zip(l1, l2))
+    print(l3)
+
+    def print_top()(filename):
+        with open(filename, 'r') as arquivo:
+            conteudo = arquivo.read()
+        l = conteudo[:].lower().split()
+        # print(l)
+        leters = []
+        nums = []
+        for w in l:
+            if w in l and w:
+                leters.append(w)
+                nums.append(l.count(w))
+                l1 = (sorted(set(leters)))
+                l2 = (sorted(set(nums)))
+                # print(l1)
+                # print(l2)
+                l3 = list(zip(l1, l2))
+        print(l3)
 
 
 # A função abaixo chama print_words() ou print_top() de acordo com os
--- teste13.py
+++ teste13.py
@@ -1,11 +1,37 @@
-arquivo = open('letras.txt', 'r')
+#arquivo = open('letras.txt', 'r')
+#conteudo = arquivo.read()
+#with open('letras.txt', 'r') as arquivo:
+#    conteudo = arquivo.read()
+#l=conteudo[:].lower().split()
+##print(l)
+#leters=[]
+#nums=[]
+#for w in l:
+#   if w in l:
+#      leters.append(w)
+#      nums.append(l.count(w))
+#      l1= (sorted(set(leters)))
+#      l2 = (sorted(set(nums)))
+#      #print(l1)
+#      #print(l2)
+#      l3=list(zip(l1, l2))
+#print (l3)
+def print_words(filename):
+    with open(filename, 'r') as arquivo:
+        conteudo = arquivo.read()
+    l = conteudo[:].lower().split()
+    # print(l)
+    leters = []
+    nums = []
+    for w in l:
+        if w in l:
+            leters.append(w)
+            nums.append(l.count(w))
+            l1 = (sorted(set(leters)))
+            l2 = (sorted(set(nums)))
+            # print(l1)
+            # print(l2)
+            l3 = list(zip(l1, l2))
+    return(l3)
 
-conteudo = arquivo.read()
-#print(conteudo)
-nl=conteudo[:].lower().split()
-print(nl)
-
-for w in nl:
-   if w in nl:
-       print 
-       print (nl.count(w))
+print(print_words('letras.txt') )
